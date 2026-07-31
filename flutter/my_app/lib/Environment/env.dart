14d903afbff8779ecde7860cd6fbd7b3abc7bbd9
User <abdulmunna.wizinoa@gmail.com>
2026-07-31 12:12:54 +0530

    modified changes in mobile

--- flutter/my_app/lib/Environment/env.dart
+++ flutter/my_app/lib/Environment/env.dart
@@ -1,4 +1,4 @@
 class AppEnv {
-  // static const String baseUrl = "https://backdigigold.wizinoa.com";
-   static const String baseUrl = "https://selective-delirium-robe.ngrok-free.dev";
+   static const String baseUrl = "https://backdigigold.wizinoa.com";
+  // static const String baseUrl = "https://selective-delirium-robe.ngrok-free.dev";
 }
